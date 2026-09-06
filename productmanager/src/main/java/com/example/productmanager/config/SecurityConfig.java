df7b1dd036d3009e4eaa8e3b0e51394822503f11
Aditya Oza <aditya0za005@gmail.com>
2026-09-06 21:12:57 +0530

    Day 6: Add basic Spring Security authentication and authorization

--- productmanager/src/main/java/com/example/productmanager/config/SecurityConfig.java
+++ productmanager/src/main/java/com/example/productmanager/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.productmanager.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -14,8 +15,10 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
-                );
+                        .requestMatchers(HttpMethod.GET,"/api/products/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(httpBasic -> {});
 
         return http.build();
     }
